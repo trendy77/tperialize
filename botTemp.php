be3f1c1b9bcb8917ac01bcef1d007d6ac2b589e9
The Hon Rev Dr Trent D.A.M-D. Fisher, Esq. DFA* <tfisher811@gmail.com>
2017-08-27 18:30:14 +1000

    social icons

---
+++ botTemp.php
@@ -0,0 +1,117 @@
+
+	<div class="container">
+    	<div class="section black">
+			<div class="row center">
+				<div class="col m4 s12">		
+   <!-- CHIP WITH IMAGE - FOR SOCIAL CONTACT?  -->
+   
+</div>
+		</div>	
+	 	</div>
+		</div>
+<div class="parallax-container valign-wrapper">
+ <div class="parallax"><img src="3.jpg" alt="background"></div>
+ </div>
+   <div class="section grey">
+ 		<div class="container light black">
+
+	 <?php include_once('contact.php'); ?>
+ 
+    	<div class="section black">
+			<div class="row center">
+				<div class="col m4 s12">		
+   <!-- CHIP WITH IMAGE - FOR SOCIAL CONTACT?  -->
+    <div class="section">
+			<div class="container light black">
+ 
+    <div class="chip">
+    <img src="timg/fb.png" alt="Facebook">
+    Facebook  </div>
+  
+    <div class="chip">
+    <img src="timg/In.png" alt="Instagram">
+    Instagram  </div>
+  
+    <div class="chip">
+    <img src="timg/me.png" alt="Messenger">
+    Messenger  </div>
+    
+    <div class="chip">
+    <img src="timg/wa.png" alt="WhatsApp">
+    WhatsApp  </div>  
+
+    <div class="chip">
+    <img src="timg/tu.png" alt="Tumblr">
+    Tumblr  </div>
+  
+    <div class="chip">
+    <img src="timg/yt.png" alt="Youtube">
+    Youtube
+  </div>
+  
+    <div class="chip">
+    <img src="timg/tw.png" alt="Twitter">
+    Twitter
+  </div>
+  
+    <div class="chip">
+    <img src="timg/pi.png" alt="Pinterest">
+    Pinterest
+  </div>
+
+    <div class="chip">
+    <img src="timg/g+.png" alt="Google+">Google+
+  </div>
+  
+    <div class="chip">
+    <img src="timg/gi.png" alt="Github">
+    Github
+  </div>
+
+    <div class="chip">
+    <img src="timg/red.png" alt="Reddit">
+    Reddit
+  </div>
+
+    <div class="chip">
+    <img src="timg/sc.png" alt="SnapChat+">Snapchat
+  </div>
+  
+    <div class="chip">
+    <img src="timg/li.png" alt="LinkedIn">
+   LinkedIn
+  </div>
+
+</div>
+		</div>	
+ 	
+
+		</div>
+	</div>
+
+	
+    <div class="row">
+	<div class="col s1">
+<img id="logo-container" type="image" src="https://orgmy.biz/orgbiz/logo.gif" height="90" width="90"></a></div><div class="col s1">
+ <img id="logo-container" type="image" src="https://orgmy.biz/fnr/logo.gif" height="90" width="90"></a></div><div class="col s1">
+	<img id="logo-container" type="image" src="https://orgmy.biz/ckww/logo.gif" height="90" width="90"></div><div class="col s1">
+	<img id="logo-container" type="image" src="https://orgmy.biz/vape/logo.gif" height="90" width="90"></a></div><div class="col s1">
+	<img id="logo-container" type="image" src="https://orgmy.biz/glo/logo.gif" height="90" width="90"></a></div><div class="col s1">
+  <img id="logo-container" type="image" src="https://orgmy.biz/glo/window.gif" height="90" width="90"></a></div><div class="col s1"> 
+<img id="logo-container" type="image" src="https://orgmy.biz/gov/logo.gif" height="90" width="90"></a></div><div class="col s1">
+ <img id="logo-container" type="image" src="https://orgmy.biz/ama/logo.gif" height="90" width="90"></a></div><div class="col s1">
+	<img id="logo-container" type="image" src="https://orgmy.biz/logowom.gif" height="90" width="90"></a></div><div class="col s1">
+	<img id="logo-container" type="image" src="https://orgmy.biz/dj/logo.gif" height="90" width="90"></a></div><div class="col s1">
+	<img id="logo-container" type="image" src="https://orgmy.biz/logosty.gif" height="90" width="90"></a></div><div class="col s1">
+  <img id="logo-container" type="image" src="https://orgmy.biz/glo/road.gif" height="90" width="90"></a></div><div class="col s1">
+  </div>
+		</div>
+		</div>
+	
+<footer>
+ <div class="footer-copyright">
+ <?php include_once('footer.php'); ?>
+ </div>
+</footer>
+<?php include_once('js.php'); ?>
+ 
---
+++ initWYOURSCR.js
@@ -0,0 +1,139 @@
+(function($) {
+    $(function() {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $(".dropdown-button").dropdown();
+        $(document).ready(function() {
+            $('.collapsible').collapsible();
+        });
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '867691370038214',
+        xfbml: true,
+        version: 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-84079763-11', 'auto');
+//ga('require', '');
+ga('send', 'pageview');
+
+// This is called with the results from FB.getLoginStatus().
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        testAPI();
+    } else {
+        // The person is not logged into your app or we are unable to tell.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';
+    }
+}
+
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+}
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '1209167032497461',
+        cookie: true, // enable cookies to allow the server to access 
+        // the session
+        xfbml: true, // parse social plugins on this page
+        version: 'v2.9' // use graph api version 2.8
+    });
+    // Now that we've initialized the JavaScript SDK, we call 
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+};
+// Load the SDK asynchronously
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+    });
+}
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        p = /^http:/.test(d.location) ? 'http' : 'https';
+    if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + '://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+})(document, 'script', 'twitter-wjs');
+
+
+
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-93539078-2', 'auto');
+ga('send', 'pageview');
+
+FB.getLoginStatus(function(response) {
+    if (response.status === 'connected') {
+        var accessToken = response.authResponse.accessToken;
+    }
+});
